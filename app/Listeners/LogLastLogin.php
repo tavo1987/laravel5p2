7682f0cd66181be0a63f9138a74ffc50daec1e55
Edwin Ramírez <tavo198718@gmail.com>
2016-05-25 21:48:45 -0500

    Update timezone and add  listeners for login  event

---
+++ app/Listeners/LogLastLogin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Log;
+
+class LogLastLogin
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Login  $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        Log::info('El usuario con email: '. $event->user->email .'ha inciado sesión');
+    }
+}
