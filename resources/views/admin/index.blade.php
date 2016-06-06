ab3299ffabd484c5aeaaa876f1f091937462e632
Edwin Ramírez <tavo198718@gmail.com>
2016-06-05 21:28:53 -0500

    Fixed layout

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-12">
                 @include('partials.alert')
                 <div class="panel panel-default">
                     <div class="panel-heading">Lista de Usuarios</div>
